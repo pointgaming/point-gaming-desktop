1c546b6982a0a4ee955d447e66a12020d6bb0b1c
Sultan Saadat <sultansaadat@gmail.com>
2013-01-25 01:24:01 +0500

    user login class

---
+++ PointGamingDesktop/UserLogin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Demo
+{
+	public class UserLogin
+	{
+		public string username { get; set; }
+		public string password { get; set; }
+	}
+}
