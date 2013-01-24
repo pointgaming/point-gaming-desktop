40bf3e3f5903232b28dec15606dc79bea9b1be0c
Sultan Saadat <sultansaadat@gmail.com>
2013-01-25 01:29:17 +0500

    json.net dll classes

---
+++ PointGamingDesktop/FailureResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Demo
+{
+	public class FailureResponse
+	{
+		public bool success { get; set; }
+		public string message { get; set; }
+	}
+}
