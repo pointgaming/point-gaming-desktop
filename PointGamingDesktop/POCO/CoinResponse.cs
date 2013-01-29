bf3cc9e3e59f2eac7b49375eae82d0405587f1b6
Sultan Saadat <sultansaadat@gmail.com>
2013-01-29 20:16:43 +0500

    changed num of coins to float in coinresponse

--- PointGamingDesktop/POCO/CoinResponse.cs
+++ PointGamingDesktop/POCO/CoinResponse.cs
@@ -8,7 +8,7 @@ namespace PointGaming.POCO
 	public class CoinResponse
 	{
 		public string _id { get; set; }
-		public int number_of_coins { get; set; }
+		public float number_of_coins { get; set; }
 		public string user_id { get; set; }
 		public string wallet_id { get; set; }
 	}
