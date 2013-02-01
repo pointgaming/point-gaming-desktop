8561641fef84395222fbe1a85540badfe71d9a26
Sultan Saadat <sultansaadat@gmail.com>
2013-02-02 03:14:03 +0500

    added friend request collection

---
+++ PointGamingDesktop/POCO/FriendRequestsCollection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PointGaming
+{
+	public class FriendRequestsCollection
+	{
+			public string _id { get; set; }
+			public string friend_request_user_id { get; set; }
+			public string user_id { get; set; }
+	}
+
+	public class FriendRequestsCollectionRootObject
+	{
+		public bool success { get; set; }
+		public List<FriendRequestsCollection> friend_requests { get; set; }
+	}
+}
