519aaa8df6febd3f2df8dcf4510b4dd1dad4baaa
Dean Gores <dmgores@gmail.com>
2013-03-05 14:53:04 -0600

    Added lobby list tab

---
+++ PointGaming.Desktop/HomeTab/LobbyListTab.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PointGaming.Desktop.HomeTab
+{
+    /// <summary>
+    /// Interaction logic for LobbyListTab.xaml
+    /// </summary>
+    public partial class LobbyListTab : UserControl
+    {
+        public LobbyListTab()
+        {
+            InitializeComponent();
+        }
+    }
+}
