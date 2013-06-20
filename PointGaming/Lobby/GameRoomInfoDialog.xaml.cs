18dbdf35d18a85e6da4cd1aacc07e1815cc4e5e2
Travis Dunn <cmd@travisdunn.com>
2013-06-20 17:04:40 +0200

    Bind game room info dialog and open room url in browser

--- PointGaming/Lobby/GameRoomInfoDialog.xaml.cs
+++ PointGaming/Lobby/GameRoomInfoDialog.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.ComponentModel;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -16,8 +17,18 @@ using PointGaming.POCO;
 
 namespace PointGaming.Lobby
 {
-    public partial class GameRoomInfoDialog : Window
+    public partial class GameRoomInfoDialog : Window, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyChanged(string propertyName)
+        {
+            var changedCallback = PropertyChanged;
+            if (changedCallback == null)
+                return;
+            var args = new PropertyChangedEventArgs(propertyName);
+            changedCallback(this, args);
+        }
+
         public SocketSession UserSession;
         public PgUser[] Members;
 
@@ -31,13 +42,20 @@ namespace PointGaming.Lobby
             Close();
         }
 
+        private void gameRoomUrl_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(URL);
+        }
+
         public static void Show(GameRoomItem item, SocketSession session)
         {
             GameRoomInfoDialog info = new GameRoomInfoDialog
             {
                 UserSession = session,
                 Title = item.DisplayName,
-                Members = item.Members
+                Members = item.Members,
+                Logo = "http://forums.pointgaming.com/assets/logo-3b643498dc7635d6ce4598843b5fcf0e.png",
+                URL = item.URL
             };
 
             System.ComponentModel.ICollectionView mv = CollectionViewSource.GetDefaultView(info.Members);
@@ -52,9 +70,30 @@ namespace PointGaming.Lobby
             get { return "19,000 points"; }
         }
 
-        public String Avatar
+        private string _url;
+        public string URL
         {
-            get { return "http://forums.pointgaming.com/assets/logo-3b643498dc7635d6ce4598843b5fcf0e.png"; }
+            get { return _url; }
+            set
+            {
+                if (value == _url)
+                    return;
+                _url = value;
+                NotifyChanged("URL");
+            }
+        }
+
+        private string _logo;
+        public string Logo
+        {
+            get { return _logo; }
+            set
+            {
+                if (value == _logo)
+                    return;
+                _logo = value;
+                NotifyChanged("Logo");
+            }
         }
     }
 }
