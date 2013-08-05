5de9d41d96771fd498b48c15f2bb358203caa0c7
dmgores <dmgores@gmail.com>
2013-08-05 18:06:18 -0500

    Update better account status every 15 minutes

---
+++ PointGaming/POCO/BetterAccount.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PointGaming.POCO
+{
+    public class BetterAccountPoco
+    {
+        public int seconds { get; set; }
+    }
+}
