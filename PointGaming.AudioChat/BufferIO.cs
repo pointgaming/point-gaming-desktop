afedf3fa4831a283db0c407b6eaa920851192a22
dmgores <dmgores@gmail.com>
2013-08-08 12:53:22 -0500

    Audio chat message lengths one byte now

--- PointGaming.AudioChat/BufferIO.cs
+++ PointGaming.AudioChat/BufferIO.cs
@@ -7,27 +7,51 @@ namespace PointGaming.AudioChat
 {
     public class BufferIO
     {
-        public static bool ReadShort(byte[] buffer, int bufferLength, ref int position, out int value)
+        public static bool ReadInt(byte[] buffer, int bufferLength, ref int position, out int value)
         {
             value = 0;
 
             if (position + 4 > bufferLength)
                 return false;
 
-            ushort a, b;
-            a = buffer[position];
-            b = buffer[position + 1];
-            value = (short)(a | (b << 8));
-            position += 2;
+            uint a, b, c, d;
+            a = buffer[position++];
+            b = buffer[position++];
+            c = buffer[position++];
+            d = buffer[position++];
+            value = (int)(a | (b << 8) | (c << 16) | (d << 24));
 
             return true;
         }
 
-        public static void WriteShort(byte[] buffer, ref int position, int value)
+        public static void WriteInt(byte[] buffer, ref int position, int value)
         {
-            buffer[position] = (byte)(value);
-            buffer[position + 1] = (byte)(value >> 8);
-            position += 2;
+            buffer[position++] = (byte)(value);
+            buffer[position++] = (byte)(value >> 8);
+            buffer[position++] = (byte)(value >> 16);
+            buffer[position++] = (byte)(value >> 24);
+        }
+
+
+        public static bool ReadU8Length(byte[] buffer, int bufferLength, ref int position, out int value)
+        {
+            value = 0;
+
+            if (position + 1 > bufferLength)
+                return false;
+
+            uint a;
+            a = buffer[position++];
+            value = (int)a;
+
+            return true;
+        }
+
+        public static void WriteU8Length(byte[] buffer, ref int position, int value)
+        {
+            if (value > byte.MaxValue)
+                throw new ArgumentException("value cannot be greater than 255");
+            buffer[position++] = (byte)(value);
         }
 
         public static bool ReadString(byte[] buffer, int bufferLength, ref int position, out string value)
@@ -35,7 +59,7 @@ namespace PointGaming.AudioChat
             value = null;
 
             int len;
-            if (!ReadShort(buffer, bufferLength, ref position, out len))
+            if (!ReadU8Length(buffer, bufferLength, ref position, out len))
                 return false;
 
             if (position + len > bufferLength)
@@ -51,7 +75,7 @@ namespace PointGaming.AudioChat
         {
             byte[] valueBytes = System.Text.Encoding.UTF8.GetBytes(value);
             var len = valueBytes.Length;
-            WriteShort(buffer, ref position, len);
+            WriteU8Length(buffer, ref position, len);
             Buffer.BlockCopy(valueBytes, 0, buffer, position, len);
             position += len;
         }
@@ -59,7 +83,7 @@ namespace PointGaming.AudioChat
         public static bool ReadUtf8String(byte[] buffer, int bufferLength, ref int position, Utf8String value)
         {
             int len;
-            if (!ReadShort(buffer, bufferLength, ref position, out len))
+            if (!ReadU8Length(buffer, bufferLength, ref position, out len))
                 return false;
 
             if (position + len > bufferLength)
@@ -76,7 +100,7 @@ namespace PointGaming.AudioChat
         public static void WriteUtf8String(byte[] buffer, ref int position, Utf8String value)
         {
             var len = value.Length;
-            WriteShort(buffer, ref position, len);
+            WriteU8Length(buffer, ref position, len);
             Buffer.BlockCopy(value.Buffer, value.Position, buffer, position, len);
             position += len;
         }
@@ -86,7 +110,7 @@ namespace PointGaming.AudioChat
             value = null;
 
             int len;
-            if (!ReadShort(buffer, bufferLength, ref position, out len))
+            if (!ReadU8Length(buffer, bufferLength, ref position, out len))
                 return false;
 
             if (position + len > bufferLength)
@@ -102,7 +126,7 @@ namespace PointGaming.AudioChat
         public static void WriteBytes(byte[] buffer, ref int position, byte[] value)
         {
             var len = value.Length;
-            WriteShort(buffer, ref position, len);
+            WriteU8Length(buffer, ref position, len);
             Buffer.BlockCopy(value, 0, buffer, position, len);
             position += len;
         }
